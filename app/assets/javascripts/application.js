e796604860c91390aac6b4a0d32e0011ed8bd56e
joanieS <joan.e.soskin@gmail.com>
2014-04-10 16:45:48 -0400

    Debugging application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require google
-//= require heatmap
-//= require heatmap-gmaps
-//= require heatmap-override
+// require google
+// require heatmap
+// require heatmap-gmaps
+// require heatmap-override
 //= require_self
