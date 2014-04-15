660f6d5187df10ac37c7bd3748f29e5400187359
wfjeff <wm.jeffries@gmail.com>
2014-04-15 15:29:39 -0400

    write perform method for stream worker

--- app/workers/stream_worker.rb
+++ app/workers/stream_worker.rb
@@ -1,4 +1,17 @@
 class StreamWorker
   include Sidekiq::Worker
-  
+
+  def perform(category = :all)
+    category == :all ? open_all_streams : open_stream(category)
+  end
+
+  private
+    def open_stream(category)
+        Scan.create(category: category).send("run_twitter_stream_#{category}")
+    end
+
+    def open_all_streams
+        categories = [:nyc, :beverages, :tech_companies, :languages]
+        categories.each { |category| open_stream(category) }
+    end
 end
