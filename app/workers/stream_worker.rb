086ad77a5430327775fefce9c31718fa096cb97f
wfjeff <wm.jeffries@gmail.com>
2014-04-15 15:53:34 -0400

    lint stream worker class

--- app/workers/stream_worker.rb
+++ app/workers/stream_worker.rb
@@ -5,13 +5,12 @@ class StreamWorker
     category == :all ? open_all_streams : open_stream(category)
   end
 
-  private
-    def open_stream(category)
-        Scan.create(category: category).send("run_twitter_stream_#{category}")
-    end
+  def open_stream(category)
+    Scan.create(category: category.to_s).send("run_twitter_stream_#{category}")
+  end
 
-    def open_all_streams
-        categories = [:nyc, :beverages, :tech_companies, :languages]
-        categories.each { |category| open_stream(category) }
-    end
+  def open_all_streams
+    categories = [:nyc, :beverages, :tech_companies, :languages]
+    categories.each { |category| open_stream(category) }
+  end
 end
