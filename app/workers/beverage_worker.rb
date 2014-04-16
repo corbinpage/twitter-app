c77492106fa0806328dffa9b1e3945a072d82d63
wfjeff <wm.jeffries@gmail.com>
2014-04-16 15:59:04 -0400

    set sidekiq worker retry to 2

--- app/workers/beverage_worker.rb
+++ app/workers/beverage_worker.rb
@@ -1,6 +1,6 @@
 class BeverageWorker
   include Sidekiq::Worker
-    sidekiq_options :retry => false, :queue => :beverage_queue
+    sidekiq_options :retry => 2, :queue => :beverage_queue
 
   def perform
     Scan.create(category: 'beverages').run_twitter_stream_beverages
--- app/workers/tech_company_worker.rb
+++ app/workers/tech_company_worker.rb
@@ -1,6 +1,6 @@
 class TechCompanyWorker
   include Sidekiq::Worker
-  sidekiq_options :retry => false, :queue => :tech_company_queue
+  sidekiq_options :retry => 2, :queue => :tech_company_queue
   
   def perform
     Scan.create(category: 'tech_companies').run_twitter_stream_tech_companies
