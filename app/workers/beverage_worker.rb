9e1df7d5d63e7f80b3003a4281f5f814346bb67f
wfjeff <wm.jeffries@gmail.com>
2014-04-16 15:05:04 -0400

    add separate queue for each worker class

--- app/workers/beverage_worker.rb
+++ app/workers/beverage_worker.rb
@@ -1,6 +1,6 @@
 class BeverageWorker
   include Sidekiq::Worker
-    sidekiq_options :retry => false 
+    sidekiq_options :retry => false, :queue => :beverage_queue
 
   def perform
     Scan.create(category: 'beverages').run_twitter_stream_beverages
--- app/workers/language_worker.rb
+++ app/workers/language_worker.rb
@@ -1,6 +1,6 @@
 class LanguageWorker
   include Sidekiq::Worker
-  sidekiq_options :retry => false 
+  sidekiq_options :retry => false, :queue => :language_queue
   
   def perform
     Scan.create(category: 'languages').run_twitter_stream_languages
