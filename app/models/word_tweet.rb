e7167dc9a0b10d58bbef456b8d0dc1313026b4b3
Daniel Kronovet <kronovet@gmail.com>
2014-04-21 16:11:14 -0400

    more cleaning on wordtweet model

--- app/models/word_tweet.rb
+++ app/models/word_tweet.rb
@@ -3,10 +3,6 @@ class WordTweet < ActiveRecord::Base
   belongs_to :tweet
   belongs_to :word
 
-  # def self.last_10k
-  #   self.order('word_tweets.id DESC').limit(10000)
-  # end
-
   def self.tech_tweets
   	self.joins(word: :word_type).
   	select("words.text as company, to_char(word_tweets.created_at, 'DD-MM-YYYY HH12:MI') as date,count(word_tweets.id) as mentions").
