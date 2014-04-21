29349ac8545fc3d35c02f6dd33dadabe2d01ab6e
Corbin Page <corbin.page@gmail.com>
2014-04-21 10:39:38 -0400

    Finished revising the Twubbles UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,9 +31,10 @@ class ApplicationController < ActionController::Base
                             .select('tweets.text as tweet_text','tweets.sentiment_score as tweet_score')
                             .where("word_Types.text ='twubbles'")
                             .where('word_tweets.created_at > ?',Time.now - 1.hour)
+                            .where('tweets.sentiment_score < ?',0)
                             .order('tweets.sentiment_score')
                             .map{|x|[x.tweet_text,x.tweet_score]}
-    @sad_tweets = tweet_text.first(tweet_text.count/2).sample(5).map{|x| x[0]}
+    @sad_tweets = tweet_text.first(tweet_text.count).sample(5).map{|x| x[0]}
     @json = {name: 'twubbles',children: twubbles_counts = 
         WordType.joins(:tweets).where(text: 'twubbles')
         .where('tweets.created_at > ?',Time.now - 10.seconds)
