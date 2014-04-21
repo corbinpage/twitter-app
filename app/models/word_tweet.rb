482c18a5c4b267147a0bf0950af8566574754183
Daniel Kronovet <kronovet@gmail.com>
2014-04-21 11:50:26 -0400

    moved to postgres and edited wordtweet model

--- app/models/word_tweet.rb
+++ app/models/word_tweet.rb
@@ -5,20 +5,9 @@ class WordTweet < ActiveRecord::Base
 
   def self.get_tech_tweets
   	self.joins(word: :word_type).
-  	select("words.text as company, #{sql_method_by_env} as date,count(word_tweets.id) as mentions").
+  	select("words.text as company, to_char(word_tweets.created_at, 'DD/MM/YYY HH12:MI') as date,count(word_tweets.id) as mentions").
   	where("word_Types.text ='tech_companies'").
   	group('company,date').map{|x|[x.company, x.date, x.mentions]}
-  end
-
-  def self.sql_method_by_env
-    case Rails.env
-    when "production"
-      "to_char(word_tweets.created_at, 'DD/MM/YYY HH12:MI')"
-    when "development"
-      "strftime('%Y-%m-%d %H:%M',word_tweets.created_at)"
-    when "test"
-      "strftime('%Y-%m-%d %H:%M',word_tweets.created_at)"
-    end
   end
 
 end
